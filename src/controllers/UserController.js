756498d83b12f17cd7c9c2a8c981ffe8b6602169
Everton Dias <esrdias@gmail.com>
2021-03-11 12:09:04 -0300

    Adicionando sistema de validacao de senhas e cadastro de usuario sem foto

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,6 +3,7 @@ const azureStorage = require('azure-storage');
 const getStream = require('into-stream');
 
 const User = require('../models/UserModel');
+const encrypt = require('../utils/encryptSha');
 const getBlobName = require('../utils/getBlobName');
 
 const blobService = azureStorage.createBlobService();
@@ -10,19 +11,32 @@ const blobService = azureStorage.createBlobService();
 class UserModel{
 	async createUser(req, res){
 		try {
-			const user = new User(req.body.name, req.body.lastName, req.body.email, req.body.city);
+			const user = new User(req.body.name, req.body.lastName, req.body.email, req.body.city, req.body.pass);
+			
+			user.pass = encrypt(user.pass);
+
+			// Cadastro da imagem de perfil
+			let imageName = '';
+			let imageContainer = '';
+
+			if(typeof req.file === 'undefined') { // cadastro de imagem padrão
+				imageName = 'default-user-image.png';
+				imageContainer = 'project';
+			} else { // envio de imagem do usuário
+				imageName = getBlobName(req.file.originalname)
+				imageContainer = process.env.IMAGES_STORAGE_CONTAINER;
+				const stream = getStream(req.file.buffer);
+				const streamLength = req.file.buffer.length;
+		
+				blobService.createBlockBlobFromStream(process.env.IMAGES_STORAGE_CONTAINER, imageName, stream, streamLength, err => {
+					if (err) {
+						handleError(err);
+						return;
+					}
+				});
+			}
 	
-			const blobName = getBlobName(req.file.originalname);
-			const stream = getStream(req.file.buffer);
-			const streamLength = req.file.buffer.length;
-	
-			blobService.createBlockBlobFromStream(process.env.IMAGES_STORAGE_CONTAINER, blobName, stream, streamLength, err => {
-				if (err) {
-					handleError(err);
-					return;
-				}
-			});
-	
+			// Envio para o servidor
 			const pool = await sql.connect(require('../config/databaseConfig'));
 			const request = pool.request();
 	
@@ -30,16 +44,17 @@ class UserModel{
 			request.input('lastName', sql.VarChar, user.lastName);
 			request.input('email', sql.VarChar, user.email);
 			request.input('city', sql.VarChar, user.city);
-			request.input('picture', sql.VarChar, `${process.env.STORAGE_URL}/${process.env.IMAGES_STORAGE_CONTAINER}/${blobName}`);
+			request.input('picture', sql.VarChar, `${process.env.STORAGE_URL}/${imageContainer}/${imageName}`);
+			request.input('pass', sql.VarChar, user.pass);
 	
-			request.query`INSERT INTO Users (Name, LastName, Email, City, Picture) VALUES 
-				(@name, @lastName, @email, @city, @picture)`;
+			request.query`INSERT INTO Users (Name, LastName, Email, City, Picture, Pass) VALUES 
+				(@name, @lastName, @email, @city, @picture, @pass)`;
 	
 			res.sendStatus(200);
-	
+			
 		} catch (err) {
 			console.error(err);
-			res.status(400).json({ error: 'Preenchimento invalido de informações!'});
+			res.status(400).json({ error: 'Preenchimento invalido de informações!', type: err});
 			return;
 		}
 	}
