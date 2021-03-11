06df6f02594d3c28d929a0845ff2c64f021bcbb5
Everton Dias <esrdias@gmail.com>
2021-03-11 13:58:22 -0300

    Renomeando variavel de ambiente

--- src/utils/encryptSha.js
+++ src/utils/encryptSha.js
@@ -1,7 +1,7 @@
 var CryptoJS = require("crypto-js");
 
 function encryptSHA(pass){
-	return CryptoJS.HmacSHA256(JSON.stringify(pass), process.env.AES_SECRET_KEY).toString();
+	return CryptoJS.HmacSHA256(JSON.stringify(pass), process.env.SHA_SECRET_KEY).toString();
 }
 
 module.exports = encryptSHA;
