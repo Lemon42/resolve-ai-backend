c3c3959a18fae509704290851fa3ef7ec6f1aed4
Everton Dias <esrdias@gmail.com>
2021-03-23 10:33:37 -0300

    Passando /login para post

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const UserController = require('./controllers/UserController');
 const userController = new UserController();
 
 routes.post('/create-user', uploadStrategy, (req, res) => userController.createUser(req, res));
-routes.get('/login', (req, res) => userController.authenticationUser(req, res));
+routes.post('/login', (req, res) => userController.authenticationUser(req, res));
 routes.delete('/logout', (req, res) => userController.logout(req, res));
 
 module.exports = routes;
