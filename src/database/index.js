79e15d20bb838634a488e0419c2e276b5c1e9a1d
Everton Dias <esrdias@gmail.com>
2021-12-20 01:30:45 -0300

    Criando funçao para fechar conexao

--- src/database/index.js
+++ src/database/index.js
@@ -9,6 +9,14 @@ const poolPromise = new sql.ConnectionPool(config)
 	})
 	.catch(err => console.log('Conexão com o banco de dados falhou!\nErro: ', err));
 
+function closeConnection(time) {
+	console.log(`Fechando a conexão com o bando de dados em ${time}seg!`);
+
+	setTimeout(() => { // Precisa do timeout para em caso de teste dar tempo da conexão ser aberta
+		sql.close();
+	}, (time * 1000));
+}
+
 module.exports = {
-	sql, poolPromise
+	sql, poolPromise, closeConnection
 }
